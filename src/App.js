b2144b3d9c994019b85e6841dbddb9bed5137e99
dhiraj <dhirajmymodak22@gmail.com>
2025-03-13 16:33:32 +0530

    seperate page home

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React, { useEffect } from "react";
-import { Element } from "react-scroll";
 import Header from "./components/header/Header";
-import HexCanvas from "./components/hexcanvas/HexCanvas";
-import About from "./components/about/About";
-import Counter from "./components/counter/Counter";
-import Process from "./components/process/Process";
-import Client from "./components/clients/Client";
-import WorkSample from "./components/work/WorkSample";
-import Faqs from "./components/faq/Faqs";
-import Services from "./components/services/Services";
-import Contact from "./components/contact/Contact";
+
 import Footer from "./components/footer/Footer";
-import ProcessMob from "./components/process/ProcessMob";
-import WorkSampleMob from "./components/work/WorkSampleMob";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Home from "./pages/Home";
 
 const App = () => {
   useEffect(() => {
@@ -32,48 +22,8 @@ const App = () => {
     <>
       <Header />
 
-      <Element name="hexcanvas">
-        <HexCanvas />
-      </Element>
-
-      <Element name="about">
-        <About />
-      </Element>
-
-      <Element name="counter">
-        <Counter />
-      </Element>
-
-      <Element name="client">
-        <Client />
-      </Element>
-
-      <Element name="services">
-        <Services />
-      </Element>
-
-      <Element name="process" className="d-none d-sm-none d-md-block">
-        <Process />
-      </Element>
-      <Element name="processmob" className="d-block d-sm-block d-md-none">
-        <ProcessMob />
-      </Element>
-
-      <Element name="work" className="d-none d-sm-block d-md-block">
-        <WorkSample />
-      </Element>
-      <Element name="workmob" className="d-block d-sm-none d-md-none">
-        <WorkSampleMob />
-      </Element>
-
-      <Element name="faqs">
-        <Faqs />
-      </Element>
-
-      <Element name="contact">
-        <Contact />
-      </Element>
-
+      <Home/>
+    
       {/* ✅ Footer Section */}
       <Footer />
     </>
