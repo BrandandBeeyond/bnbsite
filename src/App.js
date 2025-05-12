d5d1769cf25975b1010d97814908f004bd3a3774
dhiraj <dhirajmymodak22@gmail.com>
2025-05-12 15:53:32 +0530

    analytics code updated

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import Footer from "./components/footer/Footer";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Home from "./pages/Home";
-import { useLocation } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Portfolio from "./pages/Portfolio";
 
 const App = () => {
-  const TRACKING_ID = "G-60G9YGJRFT";
+  // const TRACKING_ID = "G-60G9YGJRFT";
+  const TRACKING_ID = "G-8TTH8GV3LY";
   const location = useLocation();
+
 
   useEffect(() => {
     ReactGA.initialize(TRACKING_ID);
@@ -20,24 +25,35 @@ const App = () => {
   }, [location]);
 
   useEffect(() => {
-   
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }, [location.pathname]);
+
+  useEffect(() => {
     AOS.init({
       once: true,
       duration: 700,
       easing: "ease-out-cubic",
     });
 
-
     const handleLoad = () => AOS.refresh();
     window.addEventListener("load", handleLoad);
-    
+
     return () => window.removeEventListener("load", handleLoad);
   }, []);
   return (
     <>
       <Header />
 
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
 
       {/* ✅ Footer Section */}
       <Footer />
